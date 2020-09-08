bc44b04cd885bec7bcfb8c4729529421d1483623
Samuel Carvalho <sam.scarvalho17@gmail.com>
2020-09-08 16:23:16 -0300

    adicionado saga para os pedidos

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,24 +1,31 @@
 export {
-    addIngredient,
-    removeIngredient,
-    initIngredients,
-    setIngredients,
-    fetchIngredientsFailed,
-} from './burgerBuilder';
-export {
-    purchaseBurger,
-    purchaseInit,
-    fetchOrders
-} from './order';
+  addIngredient,
+  removeIngredient,
+  initIngredients,
+  setIngredients,
+  fetchIngredientsFailed,
+} from "./burgerBuilder";
 
 export {
-    auth,
-    authStart,
-    authSuccess,
-    authFail,
-    logout,
-    logoutSucceed,
-    setAuthRedirectPath,
-    authCheckState,
-    checkAuthTimeout
-} from './auth';
+  purchaseBurger,
+  purchaseInit,
+  purchaseBurgerStart,
+  purchaseBurgerSuccess,
+  purchaseBurgerFail,
+  fetchOrders,
+  fetchOrdersStart,
+  fetchOrdersSuccess,
+  fetchOrdersFail,
+} from "./order";
+
+export {
+  auth,
+  authStart,
+  authSuccess,
+  authFail,
+  logout,
+  logoutSucceed,
+  setAuthRedirectPath,
+  authCheckState,
+  checkAuthTimeout,
+} from "./auth";
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,82 +1,46 @@
-import * as actionTypes from './actionTypes';
-import axios from '../../axios-orders';
+import * as actionTypes from "./actionTypes";
 
-export const purchaseBurgerSuccess = (orderId, orderData) => (
-    {
-        type: actionTypes.PURCHASE_BURGER_SUCCESS,
-        orderId,
-        orderData
-    }
-);
+export const purchaseBurgerSuccess = (orderId, orderData) => ({
+  type: actionTypes.PURCHASE_BURGER_SUCCESS,
+  orderId,
+  orderData,
+});
 
-export const purchaseBurgerFail = (error) => (
-    {
-        type: actionTypes.PURCHASE_BURGER_FAIL,
-        error
-    }
-);
+export const purchaseBurgerFail = (error) => ({
+  type: actionTypes.PURCHASE_BURGER_FAIL,
+  error,
+});
 
-export const purchaseBurgerStart = () => (
-    {
-        type: actionTypes.PURCHASE_BURGER_START
-    }
-);
+export const purchaseBurgerStart = () => ({
+  type: actionTypes.PURCHASE_BURGER_START,
+});
 
-export const purchaseBurger = (orderData, token) => {
-    return dispatch => {
-        dispatch(purchaseBurgerStart());
-        axios.post(`/orders.json?auth=${token}`, orderData)
-        .then(response => {
-            dispatch(purchaseBurgerSuccess(response.data.name, orderData))
-        })
-        .catch(error => {
-            dispatch(purchaseBurgerFail(error))
-        });
-    }
-};
+export const purchaseBurger = (orderData, token) => ({
+	type: actionTypes.PURCHASE_BURGER,
+	orderData,
+	token
+});
 
-export const purchaseInit = () => (
-    {
-        type: actionTypes.PURCHASE_BURGER_INIT
-    }
-);
+export const purchaseInit = () => ({
+  type: actionTypes.PURCHASE_BURGER_INIT,
+});
 
-export const fetchOrdersSuccess = (orders) => (
-    {
-        type: actionTypes.FETCH_ORDERS_SUCCESS,
-        orders,
-    }
-);
+export const fetchOrdersSuccess = (orders) => ({
+  type: actionTypes.FETCH_ORDERS_SUCCESS,
+  orders,
+});
 
-export const fetchOrdersFail = (error) => (
-    {
-        type: actionTypes.FETCH_ORDERS_FAIL,
-        error
-    }
-);
+export const fetchOrdersFail = (error) => ({
+  type: actionTypes.FETCH_ORDERS_FAIL,
+  error,
+});
 
-export const fetchOrdersStart = () => (
-    {
-        type: actionTypes.FETCH_ORDERS_START
-    }
-);
+export const fetchOrdersStart = () => ({
+  type: actionTypes.FETCH_ORDERS_START,
+});
 
-export const fetchOrders = (token, userId) => {
-    return dispatch => {
-        dispatch(fetchOrdersStart());
-        const queryParams = `?auth=${token}&orderBy="userId"&equalTo="${userId}"`
-        axios.get(`/orders.json${queryParams}`)
-            .then(res => {
-                const orders = [];
-                for (let key in res.data) {
-                    orders.push({
-                    ...res.data[key],
-                    id: key
-                    })
-                }
-                dispatch(fetchOrdersSuccess(orders));
-            })
-            .catch(err => dispatch(fetchOrdersFail(err)))
-    }
-}
-
+export const fetchOrders = (token, userId) => ({
+	type: actionTypes.FETCH_ORDERS,
+	token,
+	userId,
+});
